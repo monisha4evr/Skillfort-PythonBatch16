cbc6c558ddc94d6b81ae5a709b65eb0bada8f30e
monisha4evr <monisha.it1510@gmail.com>
2026-05-20 16:22:09 +0530

    Introduction and Basic in Python

---
+++ basic_example.py
@@ -0,0 +1,72 @@
+# input and Output Operation:
+
+# Output 
+# Type 1 Only String
+print("Hello World") # Print whatever inside the Double Quotes
+#output: Hello World
+
+# Type 2 String + Variable
+name="Arun"
+age=25
+print("Name = ",name,"Age =",age)
+#Output : Name =  Arun Age = 25
+
+#Type 3 Format String
+print(f"Name: {name}\nAge: {age}")
+"""
+Output :
+    Name: Arun
+    Age: 25
+    """
+
+# input String 
+a = input("Enter a value")
+print(a)
+"""
+Output:
+    Enter a valuemm
+    mm
+    """
+# Data type
+# id() and type() methods
+
+a = 123
+print(type(a))  # int
+print(id(a)) #memory address
+
+a = "qwr"
+print(type(a)) # str
+print(id(a))
+
+a= True
+print(type(a))  # bool
+print(id(a))
+
+a = 12.2
+print(type(a)) #float
+print(id(a))
+
+a = 3 + 4j
+print(type(a))  # complex 
+print(id(a))
+    
+    
+
+# implicit Type Conversion
+a=10 #type - int
+b=10.5 #type - Float
+c=a+b #type - Float automatically convert int to float
+
+a=5 #int
+b=5 #int
+c=a/b #float
+
+#explicit Type Conversion
+a="123"   # str
+b=int(a)  # int
+
+a=(1,2,3)
+b=list(a)
+print(b)
+print(tuple(b))
+
