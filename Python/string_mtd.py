e76f4b47e91e137ee93d9b3b0db4adfcb1f9eaa4
monisha4evr <monisha.it1510@gmail.com>
2026-06-16 13:55:31 +0530

    javascript introduction

--- Python/string_mtd.py
+++ Python/string_mtd.py
@@ -2,7 +2,7 @@ str="apple"
 print(str[::-1]) # elppa - Reverse the String
 print(str[1:5]) #pple (n-1) - slicing start and ending Index
 print(str[1]) #p - Indexing
-print(str[4:1:-2])
+print(str[4:1:-2]) #ep
 
 
 print(str.capitalize()) #Apple
@@ -19,3 +19,19 @@ print(str.split())
 
 str=['apple', 'is', 'my', 'fav']
 print(" ".join(str))
+
+str="python"
+new=str[2::]+str[:2]
+print(new)
+print(["presernt" if str in new else "not present"])
+
+a="python"
+print(a[::-1])
+print(a[6:0:-2])
+print(a[2:3])
+
+print("Hi" * 3)  # 'HiHiHi'
+print("Hi" * 0)  # ''
+
+str="flower"
+print(str.upper())
--- javascript/script1.js
+++ javascript/script1.js
@@ -1,35 +1,107 @@
-/* Scope
-     1. Global Scope
-     2. Functional Scope
-     3. Block Scope  */
+console.log("I am from External Script file")
+a=10
+console.log(`a value is ${a}`)
+
+Data Types:
+
+1. Primitive (Single value)
+2. non primitive (collection)
+
+Number
+String
+Boolean
+float
+null
+undefined
+NaN
+
+Scope
+var , let , const 
+var => Global/Functional scope
+let , const = > Block Scope
+
+var
+1. scope : Global/functional 
+2.  can redeclare
+3.  can reassign
+4.  hoisting allow
+5.  initailize not mandatory
+ 
+var a=10
+var a=20
+
+let :
+1. scope : Block 
+2. can't redeclare
+3. can reassign
+4. hosisting not allow (TDZ)
+5. initailize not mandatory
+
+
+let a=10;
+ a=20;
+
+const:
+1. Scope:Block
+2.cant redeclare
+3.can't Reassign
+4.hosisting not allow (TDZ)
+5. initailize mandatory
+
 
 const a=10;
-a=20;
+const a=20;
 
 
-DataType:
-
- int 
- float 
- string 
-
-var a="123a"
-
- null 
- undefined
-
- NaN
+Hoisting:
 
 
-function sample(){
-    a=20
-    if(true)
-    {
-        a=30
-        console.log("inside if "+a)
+console.log(a)
+b=10
+
+
+
+const a=10;
+console(a)
+
+Based on Variable Declaration
+1. Global Scope
+2. Functional Scope
+3. Block Scope
+
+const a=10;
+function fname(){
+    let b=20;
+    if(1){
+        
+        console.log("B value is"+b)
     }
-    console.log("function block" + a)
+    console.log(b)
+    console.log(a)
 }
-console.log("Outer" + a)
-sample()
+fname()
+
+
+//type coercion 
+1. implicit
+2. explicit Number(),string()
+
+a=15-"12"
+console.log(a)
+
+a="abc"+12+3
+console.log(a)
+
+a=12+5+"abc"
+console.log(a)
+
+
+
+
+
+
+
+
+
+
 
