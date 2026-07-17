0912cd0fc0c42e498266c9d67c2bceef306e8483
monisha4evr <monisha.it1510@gmail.com>
2026-07-17 10:56:16 +0530

    Django

--- Greenhouse/project/app/forms.py
+++ Greenhouse/project/app/forms.py
@@ -1,8 +1,11 @@
 from django.forms import ModelForm
 from .models import StudentDetails 
+from django import forms
 
 class StudentForm(ModelForm):
+    price = forms.IntegerField(required=True)
     class Meta:
         model = StudentDetails
         fields = "__all__"
         #fields = ['name','address','course]
+
